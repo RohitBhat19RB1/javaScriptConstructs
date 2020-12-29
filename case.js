ebcecdd37fca2a798af27b7a95b12946b03ad684
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-12-29 18:18:50 +0530

    Modified: added some more solutions

--- case.js
+++ case.js
@@ -91,3 +91,31 @@ prompts.question("enter value in unit format 1,10,100,1000 etc  : ", function (v
 process.exit();
 }); 
 
+/*Write a program that takes User Inputs and does Unit Conversion of different Length units
+1. Feet to Inch                               3. Inch to Feet
+         2. Feet to Meter                             4. Meter to Feet*/
+
+const readline = require('readline-sync');
+let num = readline.questionInt("Enter your choice for conversion: \n1. Feeet to inch " + ""
+        + "\n2. Feet to meter \n3. Inch to feet \n4. Meter to feet\n");
+switch (num) {
+        case 1:
+                let feet = readline.questionInt("Enter how many feets? ");
+                console.log(feet * 12 + " inches");
+                break;
+        case 2:
+                let feets = readline.questionInt("Enter how many feets? ");
+                console.log((feets * 100) / 328 + " Meters");
+                break;
+        case 3:
+                let inch = readline.questionInt("Enter how many inches? ");
+                console.log(inch / 12 + " feets");
+                break;
+        case 4:
+                let meter = readline.questionInt("Enter how many meters? ");
+                console.log((meter * 328) / 100 + " feets");
+                break;
+        default:
+                console.log("Invalid input");
+                break;
+}
